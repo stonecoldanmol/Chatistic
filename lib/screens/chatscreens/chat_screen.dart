be9d93a1f84a4b669e83988784a144d7c15aec54
Anmol Sinha <sinhaanmol.16@gmail.com>
2020-08-03 13:35:01 +0530

    Adding Chat Screen demo

--- lib/screens/chatscreens/chat_screen.dart
+++ lib/screens/chatscreens/chat_screen.dart
@@ -1,4 +1,6 @@
 import 'package:chatistic/models/user.dart';
+import 'package:chatistic/utils/universal_variables.dart';
+import 'package:chatistic/widgets/appbar.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -14,7 +16,56 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+        backgroundColor: UniversalVariables.blackColor,
+      appBar: customAppBar(context),
     );
   }
+
+  CustomAppBar customAppBar(context){
+    return CustomAppBar(
+      leading: IconButton(
+        icon: Icon(
+          Icons.arrow_back,
+        ),
+        onPressed: (){
+          Navigator.pop(context);
+        },
+      ),
+      centerTitle: false,
+      title: Text(
+          widget.receiver.name
+      ),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.video_call,
+          ),
+          onPressed: ()
+          {
+
+          },
+        ),
+        IconButton(
+          icon: Icon(
+            Icons.phone,
+          ),
+          onPressed: ()
+          {
+
+          },
+        ),
+
+      ],
+    );
+
+  }
 }
+
+
+
+
+
+
+
+
+
