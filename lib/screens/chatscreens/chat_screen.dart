dbd6d068e9036ca03f06fc3621f31d91d1264284
Anmol Sinha <sinhaanmol.16@gmail.com>
2020-08-03 23:48:22 +0530

    Added the static chat screen

--- lib/screens/chatscreens/chat_screen.dart
+++ lib/screens/chatscreens/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:chatistic/models/user.dart';
 import 'package:chatistic/utils/universal_variables.dart';
 import 'package:chatistic/widgets/appbar.dart';
+import 'package:chatistic/widgets/custom_tile.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -27,6 +28,13 @@ class _ChatScreenState extends State<ChatScreen> {
       appBar: customAppBar(context),
       body: Column(
         children: <Widget>[
+
+          Flexible(
+            child: messageList(),
+          ),
+
+
+
           chatControls(),
         ],
       ),
@@ -34,8 +42,99 @@ class _ChatScreenState extends State<ChatScreen> {
   }
 
 
-  Widget chatControls(){
+  Widget messageList()
+  {
 
+    return ListView.builder(
+      padding: EdgeInsets.all(10),
+      itemCount: 6,
+      itemBuilder: (context,index)
+      {
+        return chatMessageItem();
+
+
+      },
+    );
+
+  }
+
+Widget chatMessageItem()
+{
+
+  return Container(
+    margin: EdgeInsets.symmetric(vertical: 15),
+    child: Container(
+      alignment: Alignment.centerRight,
+      child: senderLayout(),
+    ),
+  );
+
+}
+
+Widget senderLayout()
+{
+
+  Radius messageRadius = Radius.circular(10);
+
+return Container(
+  margin: EdgeInsets.only(top:12),
+  constraints: BoxConstraints(
+    maxWidth: MediaQuery.of(context).size.width*0.65
+  ),
+  decoration: BoxDecoration(
+    color: UniversalVariables.senderColor,
+    borderRadius: BorderRadius.only(
+      topLeft: messageRadius,
+      topRight: messageRadius,
+      bottomLeft: messageRadius,
+    ),
+  ),
+  child: Padding(
+    padding: EdgeInsets.all(10),
+    child: Text(
+      'Hello',
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 16,
+      ),
+    ),
+  ),
+);
+}
+
+
+  Widget receiverLayout()
+  {
+
+    Radius messageRadius = Radius.circular(10);
+
+    return Container(
+      margin: EdgeInsets.only(top:12),
+      constraints: BoxConstraints(
+          maxWidth: MediaQuery.of(context).size.width*0.65
+      ),
+      decoration: BoxDecoration(
+        color: UniversalVariables.receiverColor,
+        borderRadius: BorderRadius.only(
+          bottomRight: messageRadius,
+          topRight: messageRadius,
+          bottomLeft: messageRadius,
+        ),
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Text(
+          'Hello',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget chatControls(){
 
     setWritingTo(bool val){
       setState(() {
@@ -44,18 +143,94 @@ class _ChatScreenState extends State<ChatScreen> {
     }
 
 
+    addMediaModal(context)
+    {
+        showModalBottomSheet(
+          context: context,
+          elevation : 0,
+          backgroundColor:UniversalVariables.blackColor,
+          builder: (context) {
+            return Column(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                  child: Row(
+                    children: <Widget>[
+                      FlatButton(
+                        child: Icon(
+                            Icons.close,
+                        ),
+                        onPressed: ()=>Navigator.maybePop(context),
+                      ),
+                      Expanded(
+                          child:Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              'Contents & Tools',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold
+                              ),
+                            ),
+                          ),
+                      )
+                    ],
+                  ),
+                ),
+                Flexible(
+                  child: ListView(
+                    children: <Widget>[
+                      ModalTile(
+                        title: 'Media',
+                        subtitle: 'Share Photos And Media',
+                        icon: Icons.photo,
+                      ),
+                      ModalTile(
+                          title: "File",
+                          subtitle: "Share Files",
+                          icon: Icons.tab),
+                      ModalTile(
+                          title: "Contact",
+                          subtitle: "Share Contacts",
+                          icon: Icons.contacts),
+                      ModalTile(
+                          title: "Location",
+                          subtitle: "Share A Location",
+                          icon: Icons.add_location),
+                      ModalTile(
+                          title: "Schedule Call",
+                          subtitle: "Arrange A Call And Get Reminders",
+                          icon: Icons.schedule),
+                      ModalTile(
+                          title: "Create Poll",
+                          subtitle: "Share Polls",
+                          icon: Icons.poll),
+                    ],
+                  ),
+                )
+              ],
+            );
+          }
+        );
+    }
+
+
     return Container(
       padding: EdgeInsets.all(10),
       child: Row(
         children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(5),
-            decoration: BoxDecoration(
-              gradient: UniversalVariables.fabGradient,
-              shape: BoxShape.circle,
-            ),
-            child: Icon(
-              Icons.add
+          GestureDetector(
+            onTap: () => addMediaModal(context),
+            child: Container(
+              padding: EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                gradient: UniversalVariables.fabGradient,
+                shape: BoxShape.circle,
+              ),
+              child: Icon(
+                Icons.add
+              ),
             ),
           ),
           SizedBox(width: 5,),
@@ -158,6 +333,56 @@ class _ChatScreenState extends State<ChatScreen> {
       ],
     );
 
+  }
+}
+
+class ModalTile extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final IconData icon;
+
+  const ModalTile({
+    @required this.title,
+    @required this.subtitle,
+    @required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      child: CustomTile(
+        mini: false,
+        leading: Container(
+          margin: EdgeInsets.only(right: 10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: UniversalVariables.receiverColor,
+          ),
+          padding: EdgeInsets.all(10),
+          child: Icon(
+            icon,
+            color: UniversalVariables.greyColor,
+            size: 38,
+          ),
+        ),
+        subtitle: Text(
+          subtitle,
+          style: TextStyle(
+            color: UniversalVariables.greyColor,
+            fontSize: 14,
+          ),
+        ),
+        title: Text(
+          title,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+            fontSize: 18,
+          ),
+        ),
+      ),
+    );
   }
 }
 
