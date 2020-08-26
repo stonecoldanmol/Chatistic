5b0ca688b3a559738e74cc4aaff302c123b1672e
Anmol Sinha <sinhaanmol.16@gmail.com>
2020-08-26 23:53:22 +0530

    Adding Video + Calling functionality

--- lib/screens/callscreens/pickup/pickup_screen.dart
+++ lib/screens/callscreens/pickup/pickup_screen.dart
@@ -1,12 +1,13 @@
-import 'dart:html';
+
 
 import 'package:chatistic/models/call.dart';
+import 'package:chatistic/resources/call_methods.dart';
 import 'package:flutter/material.dart';
 
 class PickupScreen extends StatelessWidget {
 
   final Call call;
-
+final CallMethods callMethods=CallMethods();
   PickupScreen({@required this.call,});
 
   @override
@@ -29,7 +30,29 @@ class PickupScreen extends StatelessWidget {
               call.callerPic,
               height: 150,
               width: 150,
-            )
+            ),
+            SizedBox(height: 15,),
+            Text(
+                call.callerName,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(height: 75,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.call_end),
+                  color: Colors.redAccent,
+                  onPressed: ()async
+                  {
+                    await callMethods.endCall(call:call);
+                  },
+                ),
+              ],
+            ),
           ],
         ),
       ),
