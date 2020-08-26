77d8ac4570a7e81dd15bcde8d43ee0f7d8c858a3
Anmol Sinha <sinhaanmol.16@gmail.com>
2020-08-27 00:30:15 +0530

    Adding Video + Calling functionality

--- lib/screens/callscreens/call_screen.dart
+++ lib/screens/callscreens/call_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:js_util';
+
 import 'package:chatistic/models/call.dart';
+import 'package:chatistic/resources/call_methods.dart';
 import 'package:flutter/material.dart';
 
 class CallScreen extends StatefulWidget {
@@ -10,8 +13,25 @@ class CallScreen extends StatefulWidget {
 }
 
 class _CallScreenState extends State<CallScreen> {
+  final CallMethods callMethods = CallMethods();
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Text(
+              'Call has been made',
+          ),
+          MaterialButton(
+            color: Colors.red,
+            child: Icon(
+              Icons.call_end,
+              //color: Colors.white,
+            ),
+            onPressed: () async => await callMethods.endCall(call: widget.call),
+          ),
+        ],
+      ),
+    );
   }
 }
