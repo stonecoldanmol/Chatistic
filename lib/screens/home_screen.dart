1898f2977ef5ecc07a3a036444cfa11083d82a84
Anmol Sinha <sinhaanmol.16@gmail.com>
2020-09-01 23:10:02 +0530

    Adding Video + Calling functionality

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:chatistic/provider/user_provider.dart';
+import 'package:chatistic/screens/callscreens/pickup/pickup_layout.dart';
 import 'package:chatistic/screens/pageviews/chat_list_screen.dart';
 import 'package:chatistic/utils/universal_variables.dart';
 import 'package:flutter/cupertino.dart';
@@ -57,72 +58,74 @@ class _HomeScreenState extends State<HomeScreen> {
     double _labelFontSize=10;
 
 
-    return Scaffold(
-      backgroundColor: UniversalVariables.blackColor,
-      body: PageView(
-        children: <Widget>[
-         Container(child: ChatListScreen(),),
-          Center(child: Text('Call Logs',style: TextStyle(color: Colors.white),)),
-          Center(child: Text('Contact Screen',style: TextStyle(color: Colors.white),)),
-        ],
-        controller: pageController,
-        onPageChanged: onPageChanged,
-        physics: NeverScrollableScrollPhysics(),
-      ),
-        bottomNavigationBar: Container(
-      child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 10),
-        child: CupertinoTabBar(
-            backgroundColor: UniversalVariables.blackColor,
-          items: <BottomNavigationBarItem>[
-
-            BottomNavigationBarItem(
-                icon: Icon(
-                    Icons.chat,
-                  color: (_page==0)?UniversalVariables.lightBlueColor:UniversalVariables.greyColor
-                ),
-              title: Text(
-                "Chats",
-                style: TextStyle(
-                  fontSize: _labelFontSize,
-                  color: (_page == 0)?UniversalVariables.lightBlueColor:Colors.grey
-                ),
-              )
-            ),
-
-            BottomNavigationBarItem(
-                icon: Icon(
-                    Icons.call,
-                    color: (_page==1)?UniversalVariables.lightBlueColor:UniversalVariables.greyColor
-                ),
-                title: Text(
-                  "Call",
-                  style: TextStyle(
-                      fontSize: _labelFontSize,
-                      color: (_page == 1)?UniversalVariables.lightBlueColor:Colors.grey
-                  ),
-                )
-            ),
-
-            BottomNavigationBarItem(
-                icon: Icon(
-                    Icons.contact_phone,
-                    color: (_page==2)?UniversalVariables.lightBlueColor:UniversalVariables.greyColor
-                ),
-                title: Text(
-                  "Contacts",
-                  style: TextStyle(
-                      fontSize: _labelFontSize,
-                      color: (_page == 2)?UniversalVariables.lightBlueColor:Colors.grey
-                  ),
-                )
-            ),
+    return PickupLayout(
+      scaffold: Scaffold(
+        backgroundColor: UniversalVariables.blackColor,
+        body: PageView(
+          children: <Widget>[
+           Container(child: ChatListScreen(),),
+            Center(child: Text('Call Logs',style: TextStyle(color: Colors.white),)),
+            Center(child: Text('Contact Screen',style: TextStyle(color: Colors.white),)),
           ],
-          onTap: navigationTapped,
-          currentIndex: _page,
+          controller: pageController,
+          onPageChanged: onPageChanged,
+          physics: NeverScrollableScrollPhysics(),
+        ),
+          bottomNavigationBar: Container(
+        child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 10),
+          child: CupertinoTabBar(
+              backgroundColor: UniversalVariables.blackColor,
+            items: <BottomNavigationBarItem>[
+
+              BottomNavigationBarItem(
+                  icon: Icon(
+                      Icons.chat,
+                    color: (_page==0)?UniversalVariables.lightBlueColor:UniversalVariables.greyColor
+                  ),
+                title: Text(
+                  "Chats",
+                  style: TextStyle(
+                    fontSize: _labelFontSize,
+                    color: (_page == 0)?UniversalVariables.lightBlueColor:Colors.grey
+                  ),
+                )
+              ),
+
+              BottomNavigationBarItem(
+                  icon: Icon(
+                      Icons.call,
+                      color: (_page==1)?UniversalVariables.lightBlueColor:UniversalVariables.greyColor
+                  ),
+                  title: Text(
+                    "Call",
+                    style: TextStyle(
+                        fontSize: _labelFontSize,
+                        color: (_page == 1)?UniversalVariables.lightBlueColor:Colors.grey
+                    ),
+                  )
+              ),
+
+              BottomNavigationBarItem(
+                  icon: Icon(
+                      Icons.contact_phone,
+                      color: (_page==2)?UniversalVariables.lightBlueColor:UniversalVariables.greyColor
+                  ),
+                  title: Text(
+                    "Contacts",
+                    style: TextStyle(
+                        fontSize: _labelFontSize,
+                        color: (_page == 2)?UniversalVariables.lightBlueColor:Colors.grey
+                    ),
+                  )
+              ),
+            ],
+            onTap: navigationTapped,
+            currentIndex: _page,
+          ),
         ),
       ),
-    ),
+      ),
     );
   }
 }
