88f4a6db8a9bf21cfa393ca2e074c0c7abf8efe9
Anmol Sinha <sinhaanmol.16@gmail.com>
2020-09-02 22:55:29 +0530

    Adding Video + Calling functionality

--- lib/screens/callscreens/call_screen.dart
+++ lib/screens/callscreens/call_screen.dart
@@ -1,8 +1,14 @@
 
 
+import 'dart:async';
+
 import 'package:chatistic/models/call.dart';
+import 'package:chatistic/provider/user_provider.dart';
 import 'package:chatistic/resources/call_methods.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:provider/provider.dart';
 
 class CallScreen extends StatefulWidget {
   final Call call;
@@ -14,6 +20,41 @@ class CallScreen extends StatefulWidget {
 
 class _CallScreenState extends State<CallScreen> {
   final CallMethods callMethods = CallMethods();
+
+  UserProvider userProvider;
+  StreamSubscription callStreamSubscription;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    addPostFrameCallBack();
+  }
+
+  addPostFrameCallBack()
+  {
+    SchedulerBinding.instance.addPostFrameCallback((_) {
+      userProvider = Provider.of<UserProvider>(context, listen: false);
+
+      callStreamSubscription = callMethods
+          .callStream(uid: userProvider.getUser.uid)
+          .listen((DocumentSnapshot ds) {
+        // defining the logic
+        switch (ds.data) {
+          case null:
+          // snapshot is null which means that call is hanged and documents are deleted
+            Navigator.pop(context);
+            break;
+
+          default:
+            break;
+        }
+      });
+    });
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
