d5a61e8fe958b832c85a2b7beca08cb770bcd06b
Anmol Sinha <sinhaanmol.16@gmail.com>
2020-10-04 00:07:04 +0530

    Added chat list and presence indicator

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:chatistic/enum/user_state.dart';
 import 'package:chatistic/provider/user_provider.dart';
+import 'package:chatistic/resources/auth_methods.dart';
 import 'package:chatistic/screens/callscreens/pickup/pickup_layout.dart';
 import 'package:chatistic/screens/pageviews/chat_list_screen.dart';
 import 'package:chatistic/utils/universal_variables.dart';
@@ -12,12 +14,12 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver{
 
 
   PageController pageController;
   int _page=0;
-
+  final AuthMethods _authMethods = AuthMethods();
   UserProvider userProvider;
 
   @override
@@ -31,10 +33,56 @@ class _HomeScreenState extends State<HomeScreen> {
       userProvider=Provider.of<UserProvider>(context,listen: false);
       userProvider.refreshUser();
 
+      _authMethods.setUserState(userId: userProvider.getUser.uid, userState: UserState.Online,);
+
     });
 
+    WidgetsBinding.instance.addObserver(this);
 
     pageController=PageController();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    WidgetsBinding.instance.removeObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state)
+  {
+    String currentUserId =
+    (userProvider != null && userProvider.getUser != null)
+        ? userProvider.getUser.uid
+        : "";
+
+    super.didChangeAppLifecycleState(state);
+
+    switch(state)
+    {
+      case AppLifecycleState.resumed:currentUserId!=null?_authMethods.setUserState(userId: currentUserId, userState: UserState.Online)
+          :print("resume state");
+      break;
+      case AppLifecycleState.inactive:
+        currentUserId != null
+            ? _authMethods.setUserState(
+            userId: currentUserId, userState: UserState.Offline)
+            : print("inactive state");
+        break;
+      case AppLifecycleState.paused:
+        currentUserId != null
+            ? _authMethods.setUserState(
+            userId: currentUserId, userState: UserState.Waiting)
+            : print("paused state");
+        break;
+      case AppLifecycleState.detached:
+        currentUserId != null
+            ? _authMethods.setUserState(
+            userId: currentUserId, userState: UserState.Offline)
+            : print("detached state");
+        break;
+    }
   }
 
   void onPageChanged(int page)
