c9f2b837ed02e48acbcdc6d54c5da1ab60bd33f5
Anmol Sinha <sinhaanmol.16@gmail.com>
2020-10-08 23:51:09 +0530

    sign out option added

--- lib/screens/pageviews/widgets/user_circle.dart
+++ lib/screens/pageviews/widgets/user_circle.dart
@@ -1,4 +1,5 @@
 import 'package:chatistic/provider/user_provider.dart';
+import 'package:chatistic/screens/pageviews/widgets/user_details_container.dart';
 import 'package:chatistic/utils/universal_variables.dart';
 import 'package:chatistic/utils/utilities.dart';
 import 'package:flutter/material.dart';
@@ -10,39 +11,47 @@ class UserCircle extends StatelessWidget {
   Widget build(BuildContext context) {
     final UserProvider userProvider = Provider.of<UserProvider>(context);
 
-    return Container(
-      height: 40,
-      width: 40,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(50),
-        color: UniversalVariables.separatorColor,
+    return GestureDetector(
+      onTap: ()=>showModalBottomSheet(
+        context: context,
+        backgroundColor: UniversalVariables.blackColor,
+        builder: (context)=>UserDetailsContainer(),
+        isScrollControlled: true,
       ),
-      child: Stack(
-        children: <Widget>[
-          Align(
-            alignment: Alignment.center,
-            child: Text(
-              Utils.getInitials(userProvider.getUser.name),
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: UniversalVariables.lightBlueColor,
-                fontSize: 13,
+      child: Container(
+        height: 40,
+        width: 40,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
+          color: UniversalVariables.separatorColor,
+        ),
+        child: Stack(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.center,
+              child: Text(
+                Utils.getInitials(userProvider.getUser.name),
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: UniversalVariables.lightBlueColor,
+                  fontSize: 13,
+                ),
               ),
             ),
-          ),
-          Align(
-            alignment: Alignment.bottomRight,
-            child: Container(
-              height: 12,
-              width: 12,
-              decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                      color: UniversalVariables.blackColor, width: 2),
-                  color: UniversalVariables.onlineDotColor),
-            ),
-          )
-        ],
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Container(
+                height: 12,
+                width: 12,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                        color: UniversalVariables.blackColor, width: 2),
+                    color: UniversalVariables.onlineDotColor),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
