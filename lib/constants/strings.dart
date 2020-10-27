561eb9773fb7ad35904813753a8c117dd91aa2c3
Anmol Sinha <sinhaanmol.16@gmail.com>
2020-10-27 22:31:27 +0530

    Adding functions

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -11,3 +11,12 @@ const String MESSAGE_TYPE_IMAGE="image";
 const String CALL_COLLECTION="call";
 
 const String CONTACTS_COLLECTION = "contacts";
+
+const String CALL_STATUS_DIALLED = "dialled";
+
+const String CALL_STATUS_RECEIVED = "received";
+
+const String CALL_STATUS_MISSED = "missed";
+
+const String noImageAvailable =
+    "https://www.esm.rochester.edu/uploads/NoPhotoAvailable.jpg";
